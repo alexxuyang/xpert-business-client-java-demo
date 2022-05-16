e28508132421571f4818353206ccc5c2e546b9f3
xuyang <yang.xu@nbltrust.com>
2022-05-16 12:35:41 +0800

    minor change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -19,7 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
-    private static final Logger logger = LoggerFactory.getLogger(Test.class);
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     class EncryptedMsg {
         private String encrypted;
